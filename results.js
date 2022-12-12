009dc12d49abdba60ee785fe6ef1a282c3ae8728
JaySchall <62366496+JaySchall@users.noreply.github.com>
2022-12-11 19:32:05 -0500

    Add files via upload
    final code

--- results.js
+++ results.js
@@ -9,37 +9,37 @@ var LCMbad = document.getElementById('LCMbad');
 window.onload = function testing(){
     var WMC = sessionStorage.getItem('WMC');
     document.getElementById('WMC').innerHTML = WMC;
-    if (WMC > 5)
+    if (WMC > 20)
     {
         WMCbad.style.display = 'block';
     }
     var DIT = sessionStorage.getItem('DIT');
     document.getElementById('DIT').innerHTML = DIT;
-    if (DIT > 5)
+    if (DIT > 8)
     {
         DITbad.style.display = 'block';
     }
     var NOC = sessionStorage.getItem('NOC');
     document.getElementById('NOC').innerHTML = NOC;
-    if (NOC > 5)
+    if (NOC > 15)
     {
         NOCbad.style.display = 'block';
     }
     var CBC = sessionStorage.getItem('CBC');
     document.getElementById('CBC').innerHTML = CBC;
-    if (CBC > 5)
+    if (CBC > 14)
     {
         CBCbad.style.display = 'block';
     }
     var RFC = sessionStorage.getItem('RFC');
     document.getElementById('RFC').innerHTML = RFC;
-    if (RFC > 5)
+    if (RFC > 15)
     {
         RFCbad.style.display = 'block';
     }
     var LCM = sessionStorage.getItem('LCM');
     document.getElementById('LCM').innerHTML = LCM;
-    if (LCM > 5)
+    if (LCM > 15)
     {
         LCMbad.style.display = 'block';
     }
